4393b253ede36b8687a9318f70df52cc5c221f68
Willyham <willdemaine@gmail.com>
2013-08-03 22:42:48 -0700

    Improved module definition

--- backbone.raphael.js
+++ backbone.raphael.js
@@ -1,7 +1,24 @@
-define(['Backbone'], function(Backbone) {
+/*
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['backbone'], function (Backbone) {
+            // Use global variables if the locals are undefined.
+            return factory(Backbone || root.Backbone);
+        });
+    } else {
+        // RequireJS isn't being used. Assume backbone is loaded in <script> tags
+        factory(Backbone);
+    }
+}(this, function (Backbone) {
+
     Backbone.RaphaelView = Backbone.View.extend({
 
-        setElement: function(element, delegate, undelegateEvents) {
+        setElement: function (element, delegate, undelegateEvents) {
             if (this.el && undelegateEvents) this.undelegateEvents();
             // el and $el will be the same, $el would have no special meaning...
             this.el = this.$el = element;
@@ -9,9 +26,9 @@ define(['Backbone'], function(Backbone) {
             return this;
         },
 
-        delegateEvents: function(events, undelegateEvents) {
+        delegateEvents: function (events, undelegateEvents) {
             if (!(events || (events = _.result(this, 'events')))) return this;
-            if(undelegateEvents) this.undelegateEvents();
+            if (undelegateEvents) this.undelegateEvents();
             for (var eventName in events) {
                 var method = events[eventName];
                 if (!_.isFunction(method)) method = this[events[eventName]];
@@ -19,11 +36,11 @@ define(['Backbone'], function(Backbone) {
 
                 method = _.bind(method, this);
                 //If it is one of the svg/vml events
-                if(this.el[eventName]){
+                if (this.el[eventName]) {
                     this.el[eventName](method);
                 }
                 // Custom events for RaphaelView object
-                else{
+                else {
                     this.on(eventName, method);
                 }
 
@@ -32,13 +49,11 @@ define(['Backbone'], function(Backbone) {
         },
 
         // Clears all callbacks previously bound to the view with `delegateEvents`.
-        undelegateEvents: function() {
-            if(this.el.type) this.el.unbindAll();
+        undelegateEvents: function () {
+            if (this.el.type) this.el.unbindAll();
             return this;
         }
     });
     return Backbone;
-});
-
-
+}));
 
